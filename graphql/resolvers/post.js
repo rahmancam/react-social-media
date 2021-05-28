0cec189acc03d5e5846672f1ac3cb2b462688699
Abdul Rahman <rahmancam@live.com>
2021-05-29 00:48:30 +0530

    Add create post, delete post and get post by Id

--- graphql/resolvers/post.js
+++ graphql/resolvers/post.js
@@ -1,17 +1,61 @@
 const PostModel = require('../../models/Post');
+const checkAuth = require('../../helpers/check-auth');
+const { AuthenticationError } = require('apollo-server');
 
 const resolver = {
     Query: {
         async getPosts() {
             try {
-                const posts = await PostModel.find()
+                const posts = await PostModel.find().sort({ createdAt: -1 })
                 return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+        },
+        async getPost(_, { postId }) {
+            try {
+                const post = await PostModel.findById(postId);
+                if (post) {
+                    return post
+                } else {
+                    throw new Error('Post not found');
+                }
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            const user = checkAuth(context);
+
+            const newPost = new PostModel({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+            return post;
+        },
+        async deletePost(_, { postId }, context) {
+            const user = checkAuth(context);
+
+            try {
+                const post = await PostModel.findById(postId);
+                if (user.username === post.username) {
+                    await (await post).delete();
+                    return 'Post deleted successfully';
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+                }
+
             } catch (err) {
                 throw new Error(err);
             }
         }
     }
-
 }
 
 module.exports = resolver;
