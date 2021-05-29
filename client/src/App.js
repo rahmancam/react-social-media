0738d29e17c86a48d9f54bc9cbb4522f38b09083
Abdul Rahman <rahmancam@live.com>
2021-05-29 12:11:17 +0530

    Add menubar and base layout pages for home, login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div>
-      <h1>Social One</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
